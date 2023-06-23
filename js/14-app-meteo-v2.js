78e65550e18c760869539b266f1e22737737b8b1
Charlene Lugus <charlene@lugus.agency>
2023-06-23 16:31:51 +0200

    add geolocation to meteo app

---
+++ js/14-app-meteo-v2.js
@@ -0,0 +1,80 @@
+let villeChoisie;
+//recevoirTemperature(villeChoisie);
+
+if('geolocation' in navigator){
+  navigator.geolocation.watchPosition((position) => {
+    //console.log(position.coords.latitude);
+    //console.log(position.coords.longitude);
+
+    const url = 'https://api.openweathermap.org/data/2.5/weather?lon=' + position.coords.longitude + '&lat=' + position.coords.latitude + '&appid=76759fd2302cb3e1dc6ff5e0a74c305d&units=metric';
+
+    let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
+    requete.open('GET', url); // Nous récupérons juste des données
+    requete.responseType = 'json'; // Nous attendons du JSON
+    requete.send(); // Nous envoyons notre requête
+  
+    // Dès qu'on reçoit une réponse, cette fonction est executée
+    requete.onload = function() {
+      if (requete.readyState === XMLHttpRequest.DONE) {
+        if (requete.status === 200) {
+          let reponse = requete.response;
+          // console.log(reponse);
+          let temperature = reponse.main.temp;
+          let ville       = reponse.name;
+          // console.log(temperature);
+          document.querySelector('#temperature_label').textContent = temperature;
+          document.querySelector('#ville').textContent = ville;
+        }
+        else {
+          alert('Un problème est intervenu, merci de revenir plus tard.');
+        }
+      }
+    }
+  }, erreur, options);
+}else{
+  villeChoisie = "Belfort";
+  recevoirTemperature(villeChoisie);
+}
+
+var options = {
+  enableHighAccuracy: true
+}
+
+
+let changerDeVille = document.querySelector('#changer');
+changerDeVille.addEventListener('click', () => {
+  villeChoisie = prompt('Quelle ville souhaitez-vous voir ?');
+  recevoirTemperature(villeChoisie);
+});
+
+function erreur(){
+  villeChoisie = "Paris";
+  recevoirTemperature(villeChoisie);
+}
+
+function recevoirTemperature(ville) {
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
+
+  let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
+  requete.open('GET', url); // Nous récupérons juste des données
+  requete.responseType = 'json'; // Nous attendons du JSON
+  requete.send(); // Nous envoyons notre requête
+
+  // Dès qu'on reçoit une réponse, cette fonction est executée
+  requete.onload = function() {
+    if (requete.readyState === XMLHttpRequest.DONE) {
+      if (requete.status === 200) {
+        let reponse = requete.response;
+        // console.log(reponse);
+        let temperature = reponse.main.temp;
+        let ville       = reponse.name;
+        // console.log(temperature);
+        document.querySelector('#temperature_label').textContent = temperature;
+        document.querySelector('#ville').textContent = ville;
+      }
+      else {
+        alert('Un problème est intervenu, merci de revenir plus tard.');
+      }
+    }
+  }
+}
